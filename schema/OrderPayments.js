4d9099761ba98aec19827434a4273c813e3830e9
Suraj Patil <surajtammapatil@gmail.com>
2022-09-16 18:39:26 +0530

    aggr added

--- schema/OrderPayments.js
+++ schema/OrderPayments.js
@@ -12,17 +12,14 @@ cube(`OrderPayments`, {
     count: {
       type: `count`,
     },
-
     paymentValue: {
       sql: `payment_value`,
       type: `sum`,
     },
-
     maxPaymentValue: {
       sql: `payment_value`,
       type: `max`,
     },
-
     avgPaymentValue: {
       sql: `payment_value`,
       type: `avg`,
@@ -36,10 +33,17 @@ cube(`OrderPayments`, {
       type: `number`,
       primaryKey: true,
     },
-
     paymentType: {
       sql: `payment_type`,
       type: `string`,
+    },
+  },
+
+  preAggregations: {
+    avgOrderValueByMonth: {
+      measures: [OrderPayments.avgPaymentValue],
+      timeDimension: Orders.orderPurchaseTimestamp,
+      granularity: `month`,
     },
   },
 
